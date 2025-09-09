790988baaab57d5e4ceebfcf8060155005511430
sumeya bishar <sumeya.hirsi@student.moringaschool.com>
2025-09-09 12:48:40 +0300

    initial commit

---
+++ funtion.js
@@ -0,0 +1,7 @@
+function introduceperson(name, age) {
+    return "Hi, I'm ${name} and I'm ${age} years old!";
+}
+const introduction1 = introduceperson("Alice", 25);
+const introduction2 = introduceperson("Bob", 30);
+
+console.log(introduction1)
